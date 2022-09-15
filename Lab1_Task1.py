7162ac203d727113824e17f27cac10cd8f7603bb
nen0n <gzdesenko38@gmail.com>
2022-09-15 12:32:29 +0300

    Lab#1

--- Lab1_Task1.py
+++ Lab1_Task1.py
@@ -6,7 +6,9 @@ parser.add_argument("Number1", type=int)
 parser.add_argument("Operator", type=str)
 parser.add_argument("Number2", type=int)
 args = parser.parse_args()
-if (args.Operator == "+" or args.Operator == "-" or args.Operator == "*"):
+if (args.Operator != "+" or args.Operator != "-" or args.Operator != "*" or args.Operator != "/"):
+    print("Not math symbol")
+elif (args.Operator == "+" or args.Operator == "-" or args.Operator == "*"):
     print("Result is:", eval(str(args.Number1) + args.Operator + str(args.Number2)))
 elif (args.Operator == "/" and args.Number2 == "0"):
     print("Division by zero")
