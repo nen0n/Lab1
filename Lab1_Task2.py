1839b3fda7ff0f302af2583180fe76944c6aa104
nen0n <gzdesenko38@gmail.com>
2022-09-15 12:52:03 +0300

    Lab#2

--- Lab1_Task2.py
+++ Lab1_Task2.py
@@ -6,18 +6,5 @@ parser.add_argument("Operator", type=str)
 parser.add_argument("Number1", type=int)
 parser.add_argument("Number2", type=int)
 args = parser.parse_args()
-if (args.Operator == "add"):
-    args.Operator = "+"
-    print(eval(str(args.Number1) + args.Operator + str(args.Number2)))
-elif (args.Operator == "sub"):
-    args.Operator = "-"
-    print(eval(str(args.Number1) + args.Operator + str(args.Number2)))
-elif (args.Operator == "multi"):
-    args.Operator = "*"
-    print(eval(str(args.Number1) + args.Operator + str(args.Number2)))
-elif (args.Operator == "div"):
-    args.Operator = "/"
-    if (args.Operator == "/" and args.Number2 == "0"):
-        print("Division by zero")
-    else:
-        print(eval(str(args.Number1) + args.Operator + str(args.Number2)))
+formula = 'print(eval(str(operator.' + args.Operator + '(' + str(args.Number1) + ', '+str(args.Number2)+'))))'
+exec(formula)
