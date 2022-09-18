96d848d9fc3880e98fda02e2dd8cefb31ed784fc
nen0n <gzdesenko38@gmail.com>
2022-09-18 15:03:51 +0300

    Lab#1

--- Lab1_Task1.py
+++ Lab1_Task1.py
@@ -2,9 +2,9 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("Number1", type=int)
+parser.add_argument("Number_1", type=int)
 parser.add_argument("Operator", type=str)
-parser.add_argument("Number2", type=int)
+parser.add_argument("Number_2", type=int)
 args = parser.parse_args()
 if (args.Operator != "+" and args.Operator != "-" and args.Operator != "*" and args.Operator != "/"):
     print("Not math symbol")
